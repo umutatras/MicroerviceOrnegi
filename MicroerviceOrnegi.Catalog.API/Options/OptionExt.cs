4d879dafec555d0eba4b626865a15a5436ea860d
umutatras <umutatras@gmail.com>
2025-09-06 23:03:53 +0300

    Options Pattern İle mongo db config ayarı yapıldı.

---
+++ MicroerviceOrnegi.Catalog.API/Options/OptionExt.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroerviceOrnegi.Catalog.API.Options
+{
+    public static class OptionExt
+    {
+        public static IServiceCollection AddOptionsExt(this IServiceCollection services)
+        {
+            services.AddOptions<MongoOption>().BindConfiguration(nameof(MongoOption)).ValidateDataAnnotations().ValidateOnStart();
+            return services;
+        }
+    }
+}
--- MicroerviceOrnegi.Catalog.API/Program.cs
+++ MicroerviceOrnegi.Catalog.API/Program.cs
@@ -1,10 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+using MicroerviceOrnegi.Catalog.API.Options;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOptionsExt();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
