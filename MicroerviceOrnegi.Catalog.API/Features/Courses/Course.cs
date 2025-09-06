f3f63b9ad825d065b28c36400ab55d7aa9d7f669
umutatras <umutatras@gmail.com>
2025-09-06 22:39:03 +0300

    Feature nesnesi oluşturuldu.

--- MicroerviceOrnegi.Catalog.API/Features/Courses/Course.cs
+++ MicroerviceOrnegi.Catalog.API/Features/Courses/Course.cs
@@ -3,15 +3,16 @@ using MicroerviceOrnegi.Catalog.API.Repositories;
 
 namespace MicroerviceOrnegi.Catalog.API.Features.Courses
 {
-    public class Course:BaseEntity
+    public class Course : BaseEntity
     {
         public string Name { get; set; } = default!;
-        public string Description { get; set; }=default!;
+        public string Description { get; set; } = default!;
         public decimal Price { get; set; }
         public Guid UserId { get; set; }
         public string? Picture { get; set; }
         public DateTime Created { get; set; }
         public Guid CategoryId { get; set; }
-        public Category Category{ get; set; }=default!;
+        public Category Category { get; set; } = default!;
+        public Feature Feature { get; set; } = default!;
     }
 }
