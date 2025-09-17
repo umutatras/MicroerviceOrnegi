d3f435c319153fbbaf60c47a4f999f601c768b45
umutatras <umutatras@gmail.com>
2025-09-17 08:42:23 +0300

    IGenericRepository interface'i oluşturuldu.

---
+++ MicroerviceOrnegi.Order.Application/Conracts/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace MicroerviceOrnegi.Order.Application.Conracts
+{
+    public interface IGenericRepository<TId, TEntity> where TEntity : class
+        where TId : struct
+    {
+        public Task<bool> AnyAsync(TId id);
+        public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<List<TEntity>> GetAllAsync();
+        Task<List<TEntity>> GetAllPagedAsync(int pageNumber, int pageSize);
+        ValueTask<TEntity> GetByIdAsync(TId id);
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        void AddAsync(TEntity entity);
+
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+
+    }
+}
