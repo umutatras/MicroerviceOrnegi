5564f2fa6f58ffb1507649bc1124dec898d15ba0
umutatras <umutatras@gmail.com>
2025-09-17 09:11:15 +0300

    CreateOrderCommand,AddressDto,PaymentDto,OrderItemDto tanımlandılar.

---
+++ MicroerviceOrnegi.Order.Application/Features/Orders/Create/CreateOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroerviceOrnegi.Order.Application.Features.Orders.Create
+{
+    public record CreateOrderCommand(float? DiscountRate,AddressDto Address,PaymentDto Payment,List<OrderItemDto> Items);
+    public record AddressDto(string Province, string District, string Street, string ZipCode, string Line);
+    public record PaymentDto(string CardNumber,string CardHolderName,string Expiration, string Cvc, decimal Amount);
+    public record OrderItemDto(Guid ProductId, string ProductName, decimal UnitPrice);
+}
