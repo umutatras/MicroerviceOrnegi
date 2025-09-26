9e92cd18d81dab93850001604d4ceb497d11c678
umutatras <umutatras@gmail.com>
2025-09-26 18:58:13 +0300

    efcore paketleri yüklendi. inmemory tercih edildi. payment entitysi oluşturuldu. appdbcontext nesnesi oluşturuldu.

--- MicroerviceOrnegi.Payment.API/Program.cs
+++ MicroerviceOrnegi.Payment.API/Program.cs
@@ -1,11 +1,17 @@
 using MicroerviceOrnegi.Payment.API;
+using MicroerviceOrnegi.Payment.API.Repositories;
 using MicroerviceOrnegi.Shared.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("payment-in-memory-db");
+});
 builder.Services.AddCommonServiceExt(typeof(PaymentAssembly));
 builder.Services.AddVersioningExt();
 var app = builder.Build();
---
+++ MicroerviceOrnegi.Payment.API/Repositories/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroerviceOrnegi.Payment.API.Repositories
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Payment>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                entity.Property(e => e.OrderCode).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Status).IsRequired();
+                entity.Property(e => e.Created).IsRequired();
+
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Payment> Payments { get; set; }
+    }
+}
