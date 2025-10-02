734e626e32c1100650be8b0d3dd78975e952374a
umutatras <umutatras@gmail.com>
2025-10-03 00:20:22 +0300

    gateway ayarı yapıldı.

--- MicroerviceOrnegi.Gateway/Program.cs
+++ MicroerviceOrnegi.Gateway/Program.cs
@@ -1,8 +1,13 @@
+using MicroerviceOrnegi.Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddAuthenticationAndAuthorizationExt(builder.Configuration);
+
 var app = builder.Build();
 app.MapReverseProxy();
 app.MapGet("/", () => "YARP (Gateway)");
-
+app.UseAuthentication();
+app.UseAuthorization(); 
 app.Run();
