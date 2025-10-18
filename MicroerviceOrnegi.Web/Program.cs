019ca28ac90c7e121ef09d2805863d36ab7586d8
umutatras <umutatras@gmail.com>
2025-10-18 20:20:21 +0300

    .

--- MicroerviceOrnegi.Web/Program.cs
+++ MicroerviceOrnegi.Web/Program.cs
@@ -5,7 +5,6 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
