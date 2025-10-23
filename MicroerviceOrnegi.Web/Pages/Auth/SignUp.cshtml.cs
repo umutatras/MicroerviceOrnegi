710402189cfb29be335f8683916331b2921ef22c
umutatras <umutatras@gmail.com>
2025-10-24 02:05:50 +0300

    SignUp OnPostAsync metodu yazıldı.

--- MicroerviceOrnegi.Web/Pages/Auth/SignUp.cshtml.cs
+++ MicroerviceOrnegi.Web/Pages/Auth/SignUp.cshtml.cs
@@ -4,11 +4,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MicroerviceOrnegi.Web.Pages.Auth
 {
-    public class SignUpModel : PageModel
+    public class SignUpModel(SignUpService signUpService) : PageModel
     {
         [BindProperty] public SignUpViewModel SignUpViewModel { get; set; } = SignUpViewModel.Empty;
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var result = await signUpService.CreateAccount(SignUpViewModel);
+
+
+            if (result.IsFail)
+            {
+                ModelState.AddModelError(string.Empty, result.Fail.Title);
+
+                if (!string.IsNullOrEmpty(result.Fail.Detail)) ModelState.AddModelError(string.Empty, result.Fail.Detail);
+
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
         }
     }
 }
