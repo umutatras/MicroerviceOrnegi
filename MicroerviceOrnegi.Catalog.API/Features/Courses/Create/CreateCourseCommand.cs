2a42e8c7e501d194e3a14009d42ab8588d9b72b9
umutatras <umutatras@gmail.com>
2025-10-24 20:02:33 +0300

    CreateCourseCommand değişiklik yapıldı

--- MicroerviceOrnegi.Catalog.API/Features/Courses/Create/CreateCourseCommand.cs
+++ MicroerviceOrnegi.Catalog.API/Features/Courses/Create/CreateCourseCommand.cs
@@ -1,5 +1,14 @@
 ﻿namespace MicroerviceOrnegi.Catalog.API.Features.Courses.Create
 {
-    public record CreateCourseCommand(string Name, string Description, decimal Price, IFormFile? Picture, Guid CategoryId) : IRequestByServiceResult<Guid>;
+
+    public record CreateCourseCommand : IRequestByServiceResult<Guid>
+    {
+        public string Name { get; init; } = null!;
+        public string Description { get; init; } = null!;
+        public decimal Price { get; init; }
+        public Guid CategoryId { get; init; }
+
+        public IFormFile? Picture { get; set; }
+    }
 
 }
