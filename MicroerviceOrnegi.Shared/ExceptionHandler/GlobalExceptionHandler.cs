1f95a9bc33758ffbfb89aafe23209f27f30e643e
umutatras <umutatras@gmail.com>
2025-10-24 22:10:15 +0300

    Microservice for GlobalExceptionHandler added

---
+++ MicroerviceOrnegi.Shared/ExceptionHandler/GlobalExceptionHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroerviceOrnegi.Shared.ExceptionHandler
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
+            CancellationToken cancellationToken)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await httpContext.Response.WriteAsJsonAsync(new ProblemDetails
+            {
+                Title = "An error occurred while processing your request",
+                Type = exception.GetType().Name,
+                Status = (int)HttpStatusCode.InternalServerError
+            }, cancellationToken);
+            return true;
+        }
+    }
+}
