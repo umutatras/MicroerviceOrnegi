abec2b43ee92f85f60f3fb3cd5f9704d75b74c3e
umutatras <umutatras@gmail.com>
2025-10-24 07:00:11 +0300

    programcs addauthlar eklendi.

--- MicroerviceOrnegi.Web/Program.cs
+++ MicroerviceOrnegi.Web/Program.cs
@@ -2,6 +2,7 @@ using MicroerviceOrnegi.Web.Extensions;
 using MicroerviceOrnegi.Web.Pages.Auth.SignIn;
 using MicroerviceOrnegi.Web.Pages.Auth.SignUp;
 using MicroerviceOrnegi.Web.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,19 @@ builder.Services.AddHttpClient<SignInService>();
 builder.Services.AddScoped<TokenService>();
 builder.Services.AddHttpContextAccessor();
 
+builder.Services.AddAuthentication(configureOption =>
+{
+    configureOption.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    configureOption.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.LoginPath = "/Auth/SignIn";
+        options.ExpireTimeSpan = TimeSpan.FromDays(60);
+        options.Cookie.Name = "UdemyNewMicroserviceWebCookie";
+        options.AccessDeniedPath = "/Auth/AccessDenied";
+    });
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -24,6 +38,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
