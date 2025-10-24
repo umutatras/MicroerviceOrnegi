26ebb59449f697bcca5ce95a1f8894e0018cbc14
umutatras <umutatras@gmail.com>
2025-10-24 07:22:07 +0300

    _Success ve Error Sayfaları eklendi. Layout düzenlendi.

--- MicroerviceOrnegi.Web/Pages/Index.cshtml.cs
+++ MicroerviceOrnegi.Web/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MicroerviceOrnegi.Web.Pages
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
