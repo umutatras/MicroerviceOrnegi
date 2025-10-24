4a859b77a094e398e201a18b9f49c038fefba3c0
umutatras <umutatras@gmail.com>
2025-10-24 21:49:23 +0300

    ProblemDetailsExtensions yazıldı.

---
+++ MicroerviceOrnegi.Web/Extensions/ProblemDetailsExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Refit;
+using System.Text.Json;
+
+namespace MicroerviceOrnegi.Web.Extensions
+{
+    public static class ProblemDetailsExtensions
+    {
+        // Extension method for logging ProblemDetails
+        public static void LogProblemDetails(this ILogger logger, ApiException? apiException)
+        {
+            if (string.IsNullOrEmpty(apiException!.Content))
+            {
+                logger.LogError(apiException.Message);
+                return;
+            }
+
+
+            var problemDetails = JsonSerializer.Deserialize<ProblemDetails>(apiException.Content!);
+
+            if (problemDetails is null) return;
+
+            logger.LogError(
+                "Problem Details: Title: {Title}, Detail: {Detail}, Status: {Status}",
+                problemDetails.Title,
+                problemDetails.Detail,
+                problemDetails.Status);
+        }
+    }
+}
