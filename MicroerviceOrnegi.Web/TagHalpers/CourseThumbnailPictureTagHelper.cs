8307e8f4ffb665d447d7ed08106539d25a08dccc
umutatras <umutatras@gmail.com>
2025-10-24 21:47:40 +0300

    CourseThumbnailPictureTagHelper oluşturuldu.

---
+++ MicroerviceOrnegi.Web/TagHalpers/CourseThumbnailPictureTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using MicroerviceOrnegi.Web.Options;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MicroerviceOrnegi.Web.TagHalpers
+{
+    public class CourseThumbnailPictureTagHelper(MicroserviceOption microserviceOption) : TagHelper
+    {
+        public string? Src { get; set; }
+
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+
+            var blankCourseThumbnailImagePath = "/images/blank_course_thumbnail.jpg";
+
+            if (string.IsNullOrEmpty(Src))
+            {
+                output.Attributes.SetAttribute("src", blankCourseThumbnailImagePath);
+            }
+            else
+            {
+                var courseThumbnailImagePath = $"{microserviceOption.File.BaseAddress}/{Src}";
+
+
+                output.Attributes.SetAttribute("src", courseThumbnailImagePath);
+            }
+
+
+            return base.ProcessAsync(context, output);
+        }
+    }
+}
