be3ffa6f2bc852189e04b9abe073efc75f35a566
umutatras <umutatras@gmail.com>
2025-10-24 19:50:59 +0300

    instuctor index eklendi.

---
+++ MicroerviceOrnegi.Web/Pages/Instructor/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MicroerviceOrnegi.Web.Pages.Instructor
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
