1892380966c4bbf2097a899857a4982929cd10d9
umutatras <umutatras@gmail.com>
2025-10-25 04:39:03 +0300

    Courses detail page eklendi.

---
+++ MicroerviceOrnegi.Web/Pages/Courses/Detail.cshtml.cs
@@ -0,0 +1,24 @@
+using MicroerviceOrnegi.Web.PageModels;
+using MicroerviceOrnegi.Web.Services;
+using MicroerviceOrnegi.Web.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MicroerviceOrnegi.Web.Pages.Courses;
+
+[AllowAnonymous]
+public class DetailModel(CatalogService catalogService) : BasePageModel
+{
+    public CourseViewModel? Course { get; set; }
+
+    public async Task<IActionResult> OnGet(Guid id)
+    {
+        var courseAsResult = await catalogService.GetCourse(id);
+
+        if (courseAsResult.IsFail) return ErrorPage(courseAsResult);
+
+        Course = courseAsResult.Data!;
+        return Page();
+    }
+}
