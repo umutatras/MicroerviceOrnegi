772acac86dc6c72b4ad7ee554c9d25264486f02d
umutatras <umutatras@gmail.com>
2025-10-25 04:30:06 +0300

    BasketPageViewModel ve BasketViewModel eklendi.

---
+++ MicroerviceOrnegi.Web/Pages/Basket/ViewModel/BasketPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace MicroerviceOrnegi.Web.Pages.Basket.ViewModel
+{
+
+    public record BasketPageViewModel
+    {
+        public List<BasketViewModelItem> Items { get; set; } = [];
+
+        private decimal TotalPrice { get; set; }
+
+        private decimal? TotalPriceByDiscountRate { get; set; }
+        public string? Coupon { get; set; }
+        public float? DiscountRate { get; set; }
+
+        public bool IsApplyDiscountCoupon => DiscountRate is > 0 && !string.IsNullOrEmpty(Coupon);
+
+        public bool HasItem => Items.Count > 0;
+
+
+        public decimal GetTotalPrice()
+        {
+            return IsApplyDiscountCoupon ? TotalPriceByDiscountRate!.Value : TotalPrice;
+        }
+
+
+        public void SetPrice(decimal totalPrice, decimal? totalPriceByDiscountRate)
+        {
+            TotalPrice = totalPrice;
+            TotalPriceByDiscountRate = totalPriceByDiscountRate;
+        }
+    }
+
+    public record BasketViewModelItem(
+        Guid Id,
+        string? PictureUrl,
+        string Name,
+        decimal Price,
+        decimal? PriceWithDiscountRate);
+}
---
+++ MicroerviceOrnegi.Web/Pages/Basket/ViewModel/BasketViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MicroerviceOrnegi.Web.Pages.Basket.ViewModel
+{
+    public record BasketViewModel(
+     float? DiscountRate,
+     string? Coupon,
+     decimal TotalPrice,
+     decimal? TotalPriceWithAppliedDiscount,
+     List<BasketItemViewModel> Items
+ )
+    {
+        public static BasketViewModel Empty()
+        {
+            return new BasketViewModel(0, string.Empty, 0, 0, []);
+        }
+    }
+}
