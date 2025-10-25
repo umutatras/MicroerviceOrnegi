09916ec7bf656a438584470f1e7fb1eafd285eef
umutatras <umutatras@gmail.com>
2025-10-25 04:24:57 +0300

    BasePageModel eklendi.

---
+++ MicroerviceOrnegi.Web/PageModels/BasePageModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using MicroerviceOrnegi.Web.Services;
+
+namespace MicroerviceOrnegi.Web.PageModels
+{
+
+    public class BasePageModel : PageModel
+    {
+        public IActionResult ErrorPage(ServiceResult serviceResult, string? redirectToPage = null)
+        {
+            TempData["Error_Title"] = serviceResult.Fail!.Title;
+            TempData["Error_Detail"] = serviceResult.Fail!.Detail;
+
+
+            if (redirectToPage is not null) return RedirectToPage(redirectToPage);
+
+
+            var validationError = serviceResult.Fail.Extensions.FirstOrDefault(x => x.Key == "errors");
+
+            if (validationError.Value is null) return Page();
+
+            var validationErrorAsDictionary = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(
+                validationError.Value!
+                    .ToString()!);
+
+            foreach (var fieldError in validationErrorAsDictionary!.SelectMany(fieldErrors => fieldErrors.Value))
+                ModelState.AddModelError(string.Empty, fieldError);
+
+            return Page();
+        }
+
+        public IActionResult SuccessPage(string message, string? redirectToPage = null)
+        {
+            TempData["Success"] = true;
+            TempData["Success_Message"] = message;
+
+            if (redirectToPage is not null) return RedirectToPage(redirectToPage);
+
+            return Page();
+        }
+    }
+}
