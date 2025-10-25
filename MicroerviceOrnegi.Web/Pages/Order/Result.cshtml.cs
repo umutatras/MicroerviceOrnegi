8f1fe6309c5fc598768a96c366553079c2db7c06
umutatras <umutatras@gmail.com>
2025-10-25 04:47:16 +0300

    Result page oluşturuldu.

---
+++ MicroerviceOrnegi.Web/Pages/Order/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MicroerviceOrnegi.Web.Pages.Order
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
