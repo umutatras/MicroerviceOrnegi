f3731e80123df0454eeb38965d2cf5675dbc131f
umutatras <umutatras@gmail.com>
2025-10-25 04:46:45 +0300

    History page oluşturuldu.

---
+++ MicroerviceOrnegi.Web/Pages/Order/History.cshtml.cs
@@ -0,0 +1,28 @@
+using MicroerviceOrnegi.Web.PageModels;
+using MicroerviceOrnegi.Web.Pages.Order.ViewModel;
+using MicroerviceOrnegi.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MicroerviceOrnegi.Web.Pages.Order
+{
+    [Authorize]
+    public class HistoryModel(OrderService orderService) : BasePageModel
+    {
+        public List<OrderHistoryViewModel> OrderHistoryList { get; set; } = null!;
+
+        public async Task<IActionResult> OnGet()
+        {
+            var response = await orderService.GetHistory();
+
+
+            if (response.IsFail) return ErrorPage(response);
+
+            OrderHistoryList = response.Data!;
+
+
+            return Page();
+        }
+    }
+}
