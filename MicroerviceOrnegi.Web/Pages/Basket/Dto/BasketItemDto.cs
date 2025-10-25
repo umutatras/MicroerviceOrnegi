060e882c9e95118cd5f0d8f43c8e52ef83ba7727
umutatras <umutatras@gmail.com>
2025-10-25 04:27:20 +0300

    BasketItemDto eklendi.

---
+++ MicroerviceOrnegi.Web/Pages/Basket/Dto/BasketItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroerviceOrnegi.Web.Pages.Basket.Dto
+{
+    public record BasketItemDto(
+       Guid Id,
+       string Name,
+       string ImageUrl,
+       decimal Price,
+       decimal? PriceByApplyDiscountRate)
+    {
+    }
+}
