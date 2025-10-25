c5d5f7a1596fefdc948b03b9f19419c121da5018
umutatras <umutatras@gmail.com>
2025-10-25 04:26:26 +0300

    Basket için gerekli ui oluşturuluyor. AddBasketRequest Dto eklend.

---
+++ MicroerviceOrnegi.Web/Pages/Basket/Dto/AddBasketRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroerviceOrnegi.Web.Pages.Basket.Dto
+{
+    public record AddBasketRequest(
+     Guid CourseId,
+     string CourseName,
+     decimal CoursePrice,
+     string? ImageUrl
+ );
+}
