9634ea38fd3c4d73c049b23fa52292dc58d3f99d
umutatras <umutatras@gmail.com>
2025-10-25 04:27:47 +0300

    BasketResponse eklendi.

---
+++ MicroerviceOrnegi.Web/Pages/Basket/Dto/BasketResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroerviceOrnegi.Web.Pages.Basket.Dto
+{
+    public record BasketResponse(
+     float? DiscountRate,
+     string? Coupon,
+     decimal TotalPrice,
+     decimal? TotalPriceWithAppliedDiscount,
+     List<BasketItemDto> Items
+ );
+}
