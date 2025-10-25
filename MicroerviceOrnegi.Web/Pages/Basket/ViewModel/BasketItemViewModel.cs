ecdac1cc84a30de220de355c4ca311df54c2fad7
umutatras <umutatras@gmail.com>
2025-10-25 04:29:09 +0300

    BasketItemViewModel eklendi.

---
+++ MicroerviceOrnegi.Web/Pages/Basket/ViewModel/BasketItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroerviceOrnegi.Web.Pages.Basket.ViewModel
+{
+    public record BasketItemViewModel(
+    Guid Id,
+    string Name,
+    string ImageUrl,
+    decimal Price,
+    decimal? PriceByApplyDiscountRate)
+    {
+    }
+}
